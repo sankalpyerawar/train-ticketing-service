c398d3d9ee076eebed4a94c2da566120aa4cfe11
Sankalp Yerawar <sankalpyerawar@gmail.com>
2025-07-18 18:12:07 +0530

    Refactor pricing package

--- src/main/java/ticketing/pricing/TicketPricing.java
+++ src/main/java/ticketing/pricing/TicketPricing.java
@@ -1,4 +1,4 @@
-package pricing;
+package ticketing.pricing;
 
 import ticketing.domain.CoachType;
 
--- src/test/java/ticketing/pricing/FixPerStationTicketPricingTest.java
+++ src/test/java/ticketing/pricing/FixPerStationTicketPricingTest.java
@@ -1,4 +1,4 @@
-package pricing;
+package ticketing.pricing;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
